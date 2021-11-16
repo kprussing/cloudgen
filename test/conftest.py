a354ba4679bc81897a1f5e4d4317ebd9f7fc18b2
Keith F. Prussing <Keith.Prussing@gtri.gatech.edu>
2021-11-16 13:12:55 -0500

    Test that reparsing a new file does not crash
    Recalling the parse method currently crashes.  To test for this, we add
    a test to check the stratocumulus input file and then load both and
    recheck the loading is unique.

--- test/conftest.py
+++ test/conftest.py
@@ -1,9 +1,55 @@
 import pathlib
 
+from typing import (
+    Dict,
+)
+
 import pytest
 
 
 @pytest.fixture
-def cirrus() -> pathlib.Path:
+def sample_dir() -> pathlib.Path:
+    """The path to the samples directory"""
+    return pathlib.Path(__file__).parent.parent / "samples"
+
+
+@pytest.fixture
+def cirrus(sample_dir) -> pathlib.Path:
     """The cirrus cloud input file"""
-    return pathlib.Path(__file__).parent.parent / "samples" / "cirrus.dat"
+    return sample_dir / "cirrus.dat"
+
+
+@pytest.fixture
+def cirrus_expected(cirrus) -> Dict[str, str]:
+    """Parse the expected cirrus cloud values"""
+    return parse_input(cirrus)
+
+
+@pytest.fixture
+def stratocumulus(sample_dir) -> pathlib.Path:
+    """The stratocumulus input file"""
+    return sample_dir / "stratocumulus.dat"
+
+
+@pytest.fixture
+def stratocumulus_expected(stratocumulus) -> pathlib.Path:
+    """Parse the expected stratocumulus cloud values"""
+    return parse_input(stratocumulus)
+
+
+def parse_input(path: pathlib.Path) -> Dict[str, str]:
+    """Parse the expected values from the file"""
+    expected = {}
+    with open(path, "r") as fid:
+        for line in fid:
+            if line.strip() == "" or line.lstrip().startswith("#"):
+                continue
+
+            try:
+                param, value = line.strip().split(" ", 1)
+            except ValueError:
+                expected[line.strip()] = "true"
+            else:
+                expected[param] = value.strip()
+
+    return expected
--- test/test_rcdata.py
+++ test/test_rcdata.py
@@ -1,40 +1,53 @@
 import pathlib
 
+from typing import (
+    Dict,
+)
+
 from cloudgen import parse_input_file
 
 
-def test_parse_input_file(cirrus: pathlib.Path) -> None:
-    """Check parsing the input file works as expected"""
+def test_parse_cirrus(cirrus: pathlib.Path,
+                      cirrus_expected: Dict[str, str]) -> None:
+    """Check parsing the cirrus input file works as expected"""
     rc_data = parse_input_file(cirrus)
     assert rc_data
-    expected = {
-        "verbose": "true",
-        "x_domain_size": "200000",
-        "z_domain_size": "3500",
-        "x_pixels": "256",
-        "z_pixels": "32",
-        "z_offset": "7000",
-        "seed": "2",
-        "vertical_exponent": "-2",
-        "outer_scale": "50000",
-        "interp_height": "7000 7500 8000 8500 9000 9500 10000 10500",
-        "v_wind": "10 15 20 25 30 35 40 40",
-        "u_wind": "5 2.5 0 -1 0 2.5 5 5",
-        "fall_speed": "1 0.9 0.8 0.7 0.6 0.5 0.4 0.3",
-        "generating_level": "9500",
-        "horizontal_exponent": "-1.667 -3 -2.5 -2 -1.667 -1.667 -1.667 -1.667",
-        "anisotropic_mixing": "true",
-        "lognormal_distribution": "true",
-        "mean": "0.0 0.06 0.05 0.04 0.02 0.01 0.0001 0",
-        "standard_deviation": "2 1 1.25 1.5 1.75 2 2.1 2.2",
-        "threshold": "0.001",
-        "missing_value": "-999.0",
-        "output_filename": "iwc.nc",
-        "variable_name": "iwc",
-        "long_name": "Ice water content",
-        "units": "g m-3",
-        "title": "3D stochastic field of ice water content",
-    }
-    for param, value in expected.items():
+    for param, value in cirrus_expected.items():
         assert param in rc_data
         assert value.strip() == rc_data[param].strip()
+
+    assert all(_ in cirrus_expected for _ in rc_data)
+
+
+def test_parse_stratocumulus(stratocumulus: pathlib.Path,
+                             stratocumulus_expected: Dict[str, str]) -> None:
+    """Check parsing the startocumulus input file works as expected"""
+    rc_data = parse_input_file(stratocumulus)
+    assert rc_data
+    for param, value in stratocumulus_expected.items():
+        assert param in rc_data
+        assert value.strip() == rc_data[param].strip()
+
+    assert all(_ in stratocumulus_expected for _ in rc_data)
+
+
+def test_reparse(cirrus: pathlib.Path,
+                 cirrus_expected: Dict[str, str],
+                 stratocumulus: pathlib.Path,
+                 stratocumulus_expected: Dict[str, str],
+                 ) -> None:
+    """Check that parsing a second file does not crash"""
+    rc1 = parse_input_file(cirrus)
+    rc2 = parse_input_file(stratocumulus)
+    assert rc1 is not rc2
+    for param, value in cirrus_expected.items():
+        assert param in rc1
+        assert value.strip() == rc1[param].strip()
+
+    assert all(_ in cirrus_expected for _ in rc1)
+
+    for param, value in stratocumulus_expected.items():
+        assert param in rc2
+        assert value.strip() == rc2[param].strip()
+
+    assert all(_ in stratocumulus_expected for _ in rc2)
